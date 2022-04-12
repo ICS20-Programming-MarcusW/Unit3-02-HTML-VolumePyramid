a9d9cc694981d6d15682dc2cb54e9f480a7733c8
MarcusW <99274214+MarcusWehbi@users.noreply.github.com>
2022-04-12 14:39:58 +0000

    Created program to calculate volume of triangular pyramid

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Copyright (c) 2022 Marcus Wehbi All rights reserved
+//
+// Created by: Marcus Wehbi
+// Created on: April 2022
+// This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates the volume of a Triangular Pyramid.
+ */
+function calcVolume () {
+  // get user input
+  let side_a = parseFloat(document.getElementById("side-a").value)
+	let side_b = parseFloat(document.getElementById("side-b").value)
+	let height = parseFloat(document.getElementById("height").value)
+
+
+  // calculate the volume
+  let volume = (1/6) * side_a * side_b * height
+
+  // display the results
+  document.getElementById("volume").innerHTML = "The volume is " + volume.toFixed(2) + "m<sup>3</sup>"
+}
